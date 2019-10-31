9a2213da3ca7b3bc841a62b90f0e9ee09130a53f
marques431 <rmarques@partner-it-group.com>
2019-10-31 13:54:26 +0000

    icon

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
   <!-- Custom styles for this template -->
   <link href="css/agency.min.css" rel="stylesheet">
   <link href="css/custom.css" rel="stylesheet">
+  <link rel="icon" href="./img/logos/wesave.png">
 
 </head>
 
